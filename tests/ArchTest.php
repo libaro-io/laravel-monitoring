e2318291ece5586b43258cc88e0ef42ab70f1d38
neemspees (Kim Bouchouaram) <kim@libaro.be>
2023-08-22 16:44:26 +0200

    Remove arch test requirement

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,9 @@
 <?php
 
+if (! interface_exists(\Pest\Arch\Contracts\ArchExpectation::class)) {
+    return;
+}
+
 it('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray', 'var_dump', 'print_r'])
     ->each->not->toBeUsed();
